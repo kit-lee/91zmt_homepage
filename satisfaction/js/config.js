bcc5c30916d6eaa500165579d800e0ca33f2a5f4
ouy <zza136055370@163.com>
2016-06-02 18:23:27 +0800

    初始化

---
+++ satisfaction/js/config.js
@@ -0,0 +1,13 @@
+var isTest = false;
+
+var config = config || {
+        base : "http://"+(isTest?"test.":"")+"platform.91zmt.com",
+        webcode : "91zmt",
+        uid : 66
+    };
+
+
+
+
+
+
